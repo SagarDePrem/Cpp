a35c6257284574b352ea4cd389f84cb06b6717fb
Prem <sagardeprem@gmail.com>
2018-09-05 19:50:48 +0530

    added queues

--- DataStructures/stacks_queues.cpp
+++ DataStructures/stacks_queues.cpp
@@ -1,6 +1,10 @@
 /* Stacks - Last In First Out data structure
  * It's like a jar of stacked cookies
  * Four operations - pop(), push(), top(),isEmpty()
+ *****************************************************
+ * Queues - First In First Out data structure
+ * Ops - peek(), isEmpty(), enqueue(), dequeue()
+ *****************************************************
  */
 #include<iostream>
 
@@ -51,8 +55,66 @@ public:
     }
 };
 
+class Queue
+{
+private:
+    struct Node
+    {
+        int data;
+        struct Node *next;
+    };
+    struct Node *head;
+    struct Node *tail;
+
+public:
+    Queue()         //constructor
+    {
+        head=NULL;
+        tail=NULL;
+    }
+    ~Queue()        //deconstructor
+    {
+        while(head!=NULL)
+        {
+            Node* toDelete=head;    // start removing from head
+            head=head->next;        // update head
+            delete toDelete;
+        }
+    }
+    bool isEmpty()  // check if stack is empty
+    {
+        return head==NULL;
+    }
+    void dequeue()
+    {
+        if (head==NULL)
+            std::cout << "Queue's empty bro!";
+        else
+            head = head->next;
+    }
+    void enqueue(int data)
+    {
+        struct Node *newNode = new Node{data};
+        if (head==NULL)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+    int peek()
+    {
+        return head->data;
+    }
+};
+
 int main()
 {
+    //
     Stack mystack;
     if (mystack.isEmpty()==1)
         std::cout << "Empty stack\n";
@@ -70,5 +132,17 @@ int main()
     std::cout << "Top of stack : " << mystack.top() <<"\n";
     mystack.pop();
     std::cout << "Top of stack after pop : " << mystack.top() <<"\n";
+
+    Queue myq;
+    if (myq.isEmpty()==1)
+        std::cout << "Empty q\n";
+    myq.enqueue(3);
+    if (myq.isEmpty()==0)
+        std::cout << "Non Empty q\n";
+    std::cout << "First in queue : " << myq.peek() <<"\n";
+    myq.enqueue(5);
+    std::cout << "First in queue : " << myq.peek() <<"\n";
+    myq.dequeue();
+    std::cout << "First in queue : " << myq.peek() <<"\n";
     return (0);
 }
