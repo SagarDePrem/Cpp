0287b7aba1b7f18236383de1a16524888456a65a
Prem <sagardeprem@gmail.com>
2018-10-04 14:36:32 +0530

    all done

---
+++ DataStructures/vinuFirstProgram.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main()
+{
+    std::cout << "Hello World!\n";
+    int x,y;
+    std::cout << "Enter first number: \n";
+    std::cin >> x;
+    std::cout << "Enter second number: \n";
+    std::cin >> y;
+    std::cout << "Sum of the two number is :" << x+y <<"\n";
+    return(0);
+}
